61df9fff36a850d2cfd5e3e5338b5a8815c0fa1a
jo-sean <joseanmanuel@gmail.com>
2021-11-17 14:57:28 -0800

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit: 10,
-  host: 'classmysql.engr.oregonstate.edu',
-  user: 'cs340_perezjos',
-  password: 'Hkdi6ton!',
-  database: 'cs340_perezjos'
-});
-module.exports.pool = pool;
