54759fbd007c56d332fe80c6860a8f767bd0af21
chrismannina <chrismannina@protonmail.com>
2021-11-18 18:20:33 -0500

    Added routing for all pages.

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, json
+from flask import Flask, render_template, request, redirect, url_for
 import os
-from flask.helpers import url_for
-from werkzeug.utils import redirect
 import database.db_connector as db
 
-# Configuration
+# from flask.helpers import url_for
+# from werkzeug.utils import redirect
+
+# ************************* CONFIGURATION *************************
 app = Flask(__name__)
 
 # Connect to database when server is started
 db_connection = db.connect_to_database()
 
-# Routes
+# ************************* ROUTING *************************
 @app.route('/')
 def root():
     """
-    Serves: home page
+    Serves: index.html page
     """
     return render_template("index.html")
 
@@ -22,12 +23,12 @@ def root():
 @app.route('/medications', methods=['GET', 'POST'])
 def medications():
     """
-    Serves: medications page
+    Serves: medications.html page
     """
 
     # GET requests -> fetching all medications data
     if request.method == 'GET':
-        query = 'SELECT * FROM `medications` ORDER BY drug_name;'
+        query = "SELECT * FROM `medications` ORDER BY `drug_name`;"
         med_info = db.execute_query(db_connection, query).fetchall()
         return render_template('medications.html', med_info=med_info)
 
@@ -40,208 +41,165 @@ def medications():
         quantity = request.form['quantity']
 
         # Define query and data
-        query = 'INSERT INTO `medications` (`medication_id`, `drug_name`, `dosage_form`, `dose_number`, `dose_unit`, `quantity`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);'
+        query = "INSERT INTO `medications` (`drug_name`, `dosage_form`, `dose_number`, `dose_unit`, `quantity`) VALUES (%s, %s, %s, %s, %s);"
         data = (drug_name, dosage_form, dose_number, dose_unit, quantity)
 
         # Insert
         db.execute_query(db_connection, query, data)
 
-        # Redirect to same webpage after form submission
-        return redirect(url_for('customers'))
+        # After form submission -> reload same page
+        return redirect(url_for('medications'))
 
 
-@app.route('/vinyls', methods=['GET', 'POST'])
-def vinyls():
-    """Serves the Vinyls page"""
+@app.route('/suppliers', methods=['GET', 'POST'])
+def suppliers():
+    """
+    Serves: suppliers.html page
+    """
 
-    # Sample data to fill first two rows on table
-    # vinyl_info = [{"product_id": 3, "album": "Back in Black", "artist": "AC/DC", "genre": "hard rock", "price": 21.67, "quantity_available": 23},
-    #              {"product_id": 5, "album": "The Dark Side of the Moon", "artist": "Pink Floyd/DC", "genre": "rock", "price": 18.99, "quantity_available": 29}]
-
-    # Handle GET requests by fetching all Customers data
+    # GET requests -> fetching all suppliers data
     if request.method == 'GET':
-        query = "SELECT * FROM `Vinyls`;"
-        vinyl_info = db.execute_query(db_connection, query).fetchall()
-        return render_template("vinyls.html", vinyl_info=vinyl_info)
+        query = "SELECT * FROM `suppliers` ORDER BY `supplier_id`;"
+        supplier_info = db.execute_query(db_connection, query).fetchall()
+        return render_template('suppliers.html', supplier_info=supplier_info)
 
-    # Handle POST requests by inserting form data from front end
+    # POST requests -> insert form data
     if request.method == 'POST':
-        album_name = request.form['albumName']
-        artist_name = request.form['artistName']
-        genre = request.form['genre']
-        price = request.form['price']
-        quantity_available = request.form['quantityAvailable']
+        name = request.form['name']
+        zip_code = request.form['zip_code']
+        phone = request.form['phone']
 
-        query = "INSERT INTO `Vinyls` (`albumName`, `artistName`, `genre`, `price`, `quantityAvailable`) VALUES (%s, %s, %s, %s, %s);"
-        data = (album_name, artist_name, genre, price, quantity_available)
+        # Define query and data
+        query = "INSERT INTO `suppliers` (`name`, `zip_code`, `phone`) VALUES (%s, %s, %s);"
+        data = (name, zip_code, phone)
+
+        # Insert
+        db.execute_query(db_connection, query, data)
+
+        # After form submission -> reload same page
+        return redirect(url_for('suppliers'))
+
+
+@app.route('/patients', methods=['GET', 'POST'])
+def patients():
+    """
+    Serves: patients.html page
+    """
+
+    # GET requests -> fetching all patients data
+    if request.method == 'GET':
+        query = "SELECT * FROM `patients` ORDER BY `last_name`;"
+        pt_info = db.execute_query(db_connection, query).fetchall()
+        return render_template('patients.html', pt_info=pt_info)
+
+    # POST requests -> insert form data
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        phone = request.form['phone']
+        street_number = request.form['street_number']
+        street_name = request.form['street_name']
+        zip_code = request.form['zip_code']
+
+        # Define query and data
+        query = "INSERT INTO `patients`(`first_name`, `last_name`, `phone`, `street_number`, `street_name`, `zip_code`) VALUES (%s, %s, %s, %s, %s, %s);"
+        data = (first_name, last_name, phone, street_number, street_name, zip_code)
+
+        # Insert
+        db.execute_query(db_connection, query, data)
+
+        # After form submission -> reload same page
+        return redirect(url_for('patients'))
+
+
+@app.route('/techs', methods=['GET', 'POST'])
+def techs():
+    """
+    Serves: techs.html page
+    """
+
+    # GET requests -> fetching all pharmacy technician data
+    if request.method == 'GET':
+        query = "SELECT * FROM `pharmacy_technicians`;"
+        tech_info = db.execute_query(db_connection, query).fetchall()
+        return render_template("techs.html", tech_info=tech_info)
+
+    # POST requests -> insert form data
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        query = "INSERT INTO `pharmacy_technicians` (`first_name`,`last_name`) VALUES (%s, %s);"
+        data = (first_name, last_name)
 
         # Execute query to insert data
         db.execute_query(db_connection, query, data)
 
         # Redirect to same webpage after form submission
-        return redirect(url_for('vinyls'))
+        return redirect(url_for('techs'))
+
+
+@app.route('/prescriptions', methods=['GET', 'POST'])
+def prescriptions():
+    """
+    Serves: prescriptions.html page
+    """
+
+    # GET requests -> fetching all prescriptions data
+    if request.method == 'GET':
+        query = "SELECT * FROM `prescriptions`;"
+        prescription_info = db.execute_query(db_connection, query).fetchall()
+        return render_template("prescriptions.html", prescription_info=prescription_info)
+
+    # POST requests -> insert form data
+    if request.method == 'POST':
+        patient_id = request.form['patient_id']
+        medication_id = request.form['medication_id']
+        quantity = request.form['quantity']
+        query = "INSERT INTO `prescriptions` (`patient_id`,`medication_id`, `quantity`) VALUES (%s, %s, %s);"
+        data = (patient_id, medication_id, quantity)
+
+        # Execute query to insert data
+        db.execute_query(db_connection, query, data)
+
+        # Redirect to same webpage after form submission
+        return redirect(url_for('prescriptions'))
 
 
 @app.route('/orders', methods=['GET', 'POST'])
 def orders():
-    """Serves the Orders page"""
+    """
+    Serves: orders.html page
+    """
 
-    # Handle GET requests by fetching all Orders data
+    # GET requests -> fetching all purchase orders data
     if request.method == 'GET':
-        customer_filter = None  # customerID filter initialized to None
+        query = "SELECT * FROM `purchase_orders`;"
+        order_info = db.execute_query(db_connection, query).fetchall()
+        return render_template('orders.html', order_info=order_info)
 
-        # default query: select the entire table
-        query_orders = "SELECT * FROM `Orders`;"
-
-        # if there is no request parameter, skip to show the entire table
-        if len(request.args) == 0:
-            pass
-
-        # Delete order by orderID
-        elif request.args.get('order_to_delete') != None:
-            order_to_delete = request.args['order_to_delete']
-            query_delete_order = "DELETE FROM `Orders` WHERE `orderID` = " + order_to_delete + ";"
-            db.execute_query(db_connection, query_delete_order)
-
-        # If 'Clear Filter' option is chosen, refresh page with no filters
-        elif request.args.get('customerID') == "":
-            return redirect(url_for('orders'))
-
-        # Otherwise select table with customerID filters applied
-        elif request.args.get('customerID') != None:
-            customer_filter = request.args['customerID']
-            query_orders = "SELECT * FROM `Orders` WHERE customerID = " + customer_filter + ";"
-
-        order_info = db.execute_query(db_connection, query_orders).fetchall()
-
-        # Fetch all customerID's for drop down menu
-        query_customers = "SELECT `customerID`, `firstName`, `lastName` FROM `Customers`;"
-        customer_names = db.execute_query(db_connection, query_customers)
-
-        # Fetch all couponID's for drop down menu
-        query_coupons = "SELECT `couponID` FROM `Coupons`;"
-        coupon_info = db.execute_query(db_connection, query_coupons)
-
-        # Fetch all orderID's for drop down menu
-        query_orderID = "SELECT `orderID` FROM `Orders`;"
-        orderID_info = db.execute_query(db_connection, query_orderID)
-        return render_template("orders.html", order_info=order_info, customer_names=customer_names,
-                               coupon_info=coupon_info, orderID_info=orderID_info, filter=customer_filter)
-
+    # POST requests -> insert form data
     if request.method == 'POST':
+        supplier_id = request.form['supplier_id']
+        employee_id = request.form['employee_id']
+        medication_id = request.form['medication_id']
+        quantity = request.form['quantity']
+        unit_price = request.form['unit_price']
+        total_price = request.form['total_price']
+        date = request.form['date']
 
-        # If add an order
-        if request.form.get('customerID') != None:
-            order_date = request.form['orderDate']
-            customer_id = request.form['customerID']
-            coupon_id = request.form['couponID']
-            order_status = request.form['orderStatus']
+        # Define query and data
+        query = "INSERT INTO `purchase_orders` (`supplier_id`, `employee_id`, `medication_id`, `quantity`, `unit_price`, `total_price`, `date`) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+        data = (supplier_id, employee_id, medication_id, quantity, unit_price, total_price, date)
 
-            # Handle case when no couponID is given
-            if coupon_id == "":
-                query = "INSERT INTO `Orders` (`orderDate`,`customerID`, `couponID`, `orderStatus`) VALUES (%s, %s, NULL, %s);"
-                data = (order_date, customer_id, order_status)
-            else:
-                query = "INSERT INTO `Orders` (`orderDate`,`customerID`, `couponID`, `orderStatus`) VALUES (%s, %s, %s, %s);"
-                data = (order_date, customer_id, coupon_id, order_status)
+        # Insert
+        db.execute_query(db_connection, query, data)
 
-            # Execute query to insert data
-            db.execute_query(db_connection, query, data)
-
-        # If update an order
-        if request.form.get('order_to_update') != None:
-            order_to_update = request.form['order_to_update']
-            orderStatus_to_update = request.form['orderStatus_to_update']
-            coupon_to_update = request.form['coupon_to_update']
-
-            # Handle case when no couponID is given
-            if coupon_to_update == "":
-                query = "UPDATE `Orders` SET `orderStatus` = %s, `couponID` = NULL WHERE `orderID` = %s;"
-                data = (orderStatus_to_update, order_to_update)
-            else:
-                query = "UPDATE `Orders` SET `orderStatus` = %s, `couponID` = %s WHERE `orderID` = %s;"
-                data = (orderStatus_to_update, coupon_to_update, order_to_update)
-
-            # Execute query to update data
-            db.execute_query(db_connection, query, data)
-        # Redirect to same webpage after form submission
+        # After form submission -> reload same page
         return redirect(url_for('orders'))
 
 
-@app.route('/coupons', methods=['GET', 'POST'])
-def coupons():
-    """Serves the Coupons Products page"""
-
-    # Sample data to fill first two rows on table
-    # coupon_info = [{"coupon_id": "FALL_2021", "discount": 0.25, "expire_date": "2021-11-23"},
-    #               {"coupon_id": "FOOTOWN_SPECIAL", "discount": 0.15, "expire_date": "2021-10-31"}]
-
-    # Handle GET requests by fetching all Customers data
-    if request.method == 'GET':
-        query = "SELECT * FROM `Coupons`;"
-        coupon_info = db.execute_query(db_connection, query).fetchall()
-        return render_template("coupons.html", coupon_info=coupon_info)
-
-    # Handle POST requests by inserting form data from front end
-    if request.method == 'POST':
-        couponID = request.form['couponID']
-        percentDiscount = request.form['percentDiscount']
-        expirationDate = request.form['expirationDate']
-
-        query = "INSERT INTO `Coupons` VALUES (%s, %s, %s);"
-        data = (couponID, percentDiscount, expirationDate)
-
-        # Execute query to insert data
-        db.execute_query(db_connection, query, data)
-
-        # Redirect to same webpage after form submission
-        return redirect(url_for('coupons'))
-
-
-@app.route('/orderProducts', methods=['GET', 'POST'])
-def order_products():
-    """Serves the Order Products page"""
-
-    # Handle GET requests by fetching all Customers data
-    if request.method == 'GET':
-        query = "SELECT * FROM `OrderProducts`;"
-        orderProducts_info = db.execute_query(db_connection, query).fetchall()
-
-        # Fetch all orderID's for drop down menu
-        query_orders = "SELECT `orderID` FROM `Orders`;"
-        orders_info = db.execute_query(db_connection, query_orders)
-
-        # Fetch all productID's for drop down menu
-        query_products = "SELECT `productID` FROM `Vinyls`;"
-        products_info = db.execute_query(db_connection, query_products)
-        return render_template("order-products.html", \
-                               orderProducts_info=orderProducts_info, \
-                               orders_info=orders_info, \
-                               products_info=products_info)
-
-    # Handle POST requests by inserting form data from front end
-    if request.method == 'POST':
-        orderID = request.form['orderID']
-        productID = request.form['productID']
-        quantity = request.form['quantity']
-
-        query = "INSERT INTO `OrderProducts` VALUES (%s, %s, %s);"
-        data = (orderID, productID, quantity)
-
-        # Execute query to insert data
-        db.execute_query(db_connection, query, data)
-
-        # Redirect to same webpage after form submission
-        return redirect(url_for('order_products'))
-
-    # TODO DELETE OrderProducts
-
-
-# Listener
+# ************************* LISTENER *************************
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8796))
-    #                                 ^^^^
-    #              You can replace this number with any valid port
-
+    port = int(os.environ.get('PORT', 7888))
     app.run(port=port, debug=True)
 
